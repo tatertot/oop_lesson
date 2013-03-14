103584bedccd132acfce902668c6a4c54425ceeb
ChrUbuntu User <user@chromebox-002.(none)>
2013-03-13 18:20:11 -0700

    update game

--- engine.py
+++ engine.py
@@ -5,7 +5,7 @@ from pyglet.window import key
 from core import GameElement
 
 SCREEN_X = 800
-SCREEN_Y = 700
+SCREEN_Y = 800
 
 game_window = pyglet.window.Window(SCREEN_X, SCREEN_Y)
 
@@ -40,7 +40,8 @@ def setup_images():
             "Cat": "Character Cat Girl.png",
             "Horns": "Character Horn Girl.png",
             "Girl": "Character Pink Girl.png",
-            "Princess": "Character Princess Girl.png"
+            "Princess": "Character Princess Girl.png",
+            "Courtney": "courtney.jpg"
             }
 
     for k,v in filenames.items():
--- game.py
+++ game.py
@@ -11,13 +11,87 @@ KEYBOARD = None
 PLAYER = None
 ######################
 
+BOY = None
 GAME_WIDTH = 8
 GAME_HEIGHT = 8
 
 #### Put class definitions here ####
+class Character(GameElement):
+    IMAGE = "Horns"
+    
+    def __init__(self):
+        GameElement.__init__(self)
+        self.inventory = []
+        
+    def next_pos(self, direction):
+        if direction == "up":
+            return (self.x, self.y-1)
+        elif direction == "down":
+            return (self.x, self.y+1)
+        elif direction == "left":
+            return (self.x-1, self.y)
+        elif direction == "right":
+            return (self.x+1, self.y)
+
+class Boy(Character):
+    IMAGE = "Boy"
+    SOLID = True
+
+    def interact(self,player):
+        GAME_BOARD.draw_msg("Bonjour, Je m'appelle Pierre.")
+        GAME_BOARD.del_el(self.x,self.y)
+        GAME_BOARD.set_el((player.x-1),(player.y), self)
+
+class Heart(GameElement):
+    IMAGE = "Heart"
+    SOLID = False
+    
+    def __init__(self):
+        GameElement.__init__(self)
+        self.timer = 0
+
+    def update(self,dt):
+        self.timer += dt
+        if self.x == PLAYER.x and self.y == PLAYER.y:
+            GAME_BOARD.draw_msg("Aw, trapped by love! Now you lose everything. Especially your pride. KEEP TRYING (hint: go to the door!")
+            GAME_BOARD.del_el(BOY.x,BOY.y)
+
+        if self.timer > .5:
+            if self.y < 7:
+                GAME_BOARD.del_el(self.x,self.y)
+                GAME_BOARD.set_el(self.x,self.y+1,self)
+                self.timer = 0
+            else:
+                GAME_BOARD.del_el(self.x,self.y)
+                #GAME_BOARD.set_el(self.x-2,1,self)
+
+    
+    
+
+
+    def interact(self, player):
+        GAME_BOARD.draw_msg("Can you dodge the pain of love?")
+
+        # GAME_BOARD.del_el(self.x,self.y)
+        # GAME_BOARD.set_el((player.x-1),(player.y), self)
+
 class Rock(GameElement):
     IMAGE = "Rock"
     SOLID = True
+    def interact(self, player):
+        GAME_BOARD.draw_msg("Ouch!") 
+
+
+class Block(GameElement):
+    IMAGE = "Block"
+    SOLID = True
+    def interact(self, player):
+        GAME_BOARD.register(self)
+        if KEYBOARD[key.DOWN]:
+            GAME_BOARD.del_el(self.x,self.y)
+            GAME_BOARD.set_el((player.x-1),(player.y+1), self)
+        else:
+            GAME_BOARD.draw_msg("Ouch!!!")
 
 class BlueGem(GameElement):
     IMAGE = "BlueGem"
@@ -48,15 +122,27 @@ class Chest(GameElement):
             #if key in thing:
             self.SOLID = False   
             GAME_BOARD.draw_msg("Chest is opened. Here is your Prince Charming!")
-            boy = Boy()
-            GAME_BOARD.register(boy)
-            GAME_BOARD.set_el(6,5, boy)
+            GAME_BOARD.del_el(2,1)
+            GAME_BOARD.del_el(1,2)
+            GAME_BOARD.del_el(3,2)
+            GAME_BOARD.del_el(1,3)
+            global BOY
+            BOY = Boy()
+            GAME_BOARD.register(BOY)
+            GAME_BOARD.set_el(6,5, BOY)
+
+            heart = Heart()
+            GAME_BOARD.register(heart)
+            GAME_BOARD.set_el(4,1,heart)
+            # i= 0
+            # while i < 5:
+            #     print heart.x, heart.y
+            #     GAME_BOARD.del_el(heart.x,heart.y)
+            #     GAME_BOARD.set_el(heart.x,heart.y+1,heart)
+            #     i += 1
         else:
             GAME_BOARD.draw_msg("You must find the right key to open the chest!")
 
-class Boy(GameElement):
-    IMAGE = "Boy"
-    SOLID = True
 
 class DoorClosed(GameElement):
     IMAGE = "DoorClosed"
@@ -65,35 +151,24 @@ class DoorClosed(GameElement):
         if "OrangeGem" in player.inventory:
             #if key in thing:
                
-            GAME_BOARD.draw_msg("Door is now opened.")
+            GAME_BOARD.draw_msg("Door is now opened. Enter the tunnel of love.")
             dooropen = DoorOpen()
             GAME_BOARD.register(dooropen)
-            GAME_BOARD.set_el(3,6, dooropen)
+            GAME_BOARD.set_el(1,6, dooropen)
+            
 
 class DoorOpen(GameElement):
     IMAGE = "DoorOpen"
     SOLID = True
+    def interact(self,player):
+        GAME_BOARD.draw_msg("You have avoided romantic tragedy. Also, you saved your mega hunk from the trunk of death. YOU WIN!")
 
     # def interact(self, player):
         # player.inventory.append(self)
         # GAME_BOARD.draw_msg("You have just acquired a key! You have %d items!" %(len(player.inventory)))
 
-class Character(GameElement):
-    IMAGE = "Horns"
-    
-    def __init__(self):
-        GameElement.__init__(self)
-        self.inventory = []
-        
-    def next_pos(self, direction):
-        if direction == "up":
-            return (self.x, self.y-1)
-        elif direction == "down":
-            return (self.x, self.y+1)
-        elif direction == "left":
-            return (self.x-1, self.y)
-        elif direction == "right":
-            return (self.x+1, self.y)
+
+
 
 ####   End class definitions    ####
 
@@ -102,8 +177,7 @@ def initialize():
     rock_positions = [
             (2,1),
             (1,2),
-            (3,2),
-            (2,3)
+            (3,2)
         ]
    
     rocks = []
@@ -113,7 +187,6 @@ def initialize():
         GAME_BOARD.set_el(pos[0], pos[1], rock)
         rocks.append(rock)
 
-    rocks[-1].SOLID = False
 
     global PLAYER
     PLAYER = Character()
@@ -143,7 +216,12 @@ def initialize():
 
     doorclosed = DoorClosed()
     GAME_BOARD.register(doorclosed)
-    GAME_BOARD.set_el(3,6, doorclosed)
+    GAME_BOARD.set_el(1,6, doorclosed)
+
+    block = Block()
+    GAME_BOARD.register(block)
+    GAME_BOARD.set_el(2,3, block)
+
 
 # keyboard commands
 
@@ -181,6 +259,11 @@ def keyboard_handler():
         elif existing_el is None or not existing_el.SOLID:
             GAME_BOARD.del_el(PLAYER.x, PLAYER.y)
             GAME_BOARD.set_el(next_x, next_y, PLAYER)
+            try:
+                GAME_BOARD.del_el(BOY.x, BOY.y)
+                GAME_BOARD.set_el(next_x+1, next_y, BOY)
+            except:
+                pass
 
 
 
